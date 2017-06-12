69c111a5fa36ee5512cbbff5f18efabd905300b0
Hetelekides, Stergios <hetelek@gmail.com>
2017-06-12 16:57:26 -0700

    Initial Commit

---
+++ VisionExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VisionExample
+//
+//  Created by Hetelekides, Stergios on 6/12/17.
+//  Copyright © 2017 Hetelekides, Stergios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
